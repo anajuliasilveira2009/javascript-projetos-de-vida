97538a951d74b271c6b037e6977dfe51d464ac3d
anajuliasilveira2009 <silveira.ana07@escola.pr.gov.br>
2025-07-29 08:00:18 -0300

    return

--- main.js
+++ main.js
@@ -11,3 +11,23 @@ for(let i = 0; i< botoes.length; i++){
         textos[i].classList.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivol = new Date("2025-10005T00:00:00");
+
+contadores[0].textContent = tempoObjetivol - tempoAtual;
+
+function calculaTempo(tempoObjetivol){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivol- tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + " segundos ";
+}
